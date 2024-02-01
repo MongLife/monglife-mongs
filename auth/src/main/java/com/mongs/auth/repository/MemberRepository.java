95b5cca859f5fb57c6989d7d6ec2259215d2aa72
박상민 <87235273+tableMinPark@users.noreply.github.com>
2024-02-01 16:59:21 +0900

    회원 레포지토리 생성 및 단위 테스트 (#29)
    - 회원 레포지토리 생성
    - 회원 엔티티의 id, createdAt (가입 일자), updatedAt (회원 정보 수정 일자) 자동 입력 단위 테스트 진행
    - 딜레이 테스트 케이스를 위한 awaitility.org.awaitility:awaitility:4.2.0 의존성 추가

---
+++ auth/src/main/java/com/mongs/auth/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.mongs.auth.repository;
+
+import com.mongs.auth.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
