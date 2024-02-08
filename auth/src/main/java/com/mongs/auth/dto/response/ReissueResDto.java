d3edac57ab646db018847285e9ea65216fd2e6cf
박상민A <tableminpark@wisenut.co.kr>
2024-02-08 13:35:35 +0900

    feat: 로그인 앤드포인트 작성
    - 로그인을 위한 앤드포인트 생성
    - /auth/login 으로 진입
    - @Validate 를 통해 파라미터 유효성 확인

--- auth/src/main/java/com/mongs/auth/dto/response/ReissueResDto.java
+++ auth/src/main/java/com/mongs/auth/dto/response/ReissueResDto.java
@@ -3,5 +3,8 @@ package com.mongs.auth.dto.response;
 import lombok.Builder;
 
 @Builder
-public record ReissueResDto(String accessToken, String refreshToken) {
+public record ReissueResDto(
+        String accessToken,
+        String refreshToken
+) {
 }
