7a8984be69cb8cc0f50671a02d5c908eb2744d97
박상민A <tableminpark@wisenut.co.kr>
2024-02-13 16:06:00 +0900

    feat: 회원 토큰 재발행 앤드포인트 개발
    - 토큰 재발행 앤드포인트 생성
    - 토큰 재발행 예외 처리 부분 ErrorCode 를 통한 에러 처리 방식으로 변경
    - ErrorCode 에 REFRESH_TOKEN_EXPIRED 추가

--- auth/src/main/java/com/mongs/auth/exception/ErrorCode.java
+++ auth/src/main/java/com/mongs/auth/exception/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    INVALID_PARAMETER("ERROR_100", "Invalid Parameter");
+    INVALID_PARAMETER("100", "Invalid Parameter"),
+    REFRESH_TOKEN_EXPIRED("101", "RefreshToken Expired");
     
     private final String code;
     private final String message;
