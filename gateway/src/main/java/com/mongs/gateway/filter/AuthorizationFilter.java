767cd1ba2d0b5bbeec23b65b06804d95e41fc34f
tableminpark <tablemin_park@daum.net>
2024-02-19 00:30:54 +0900

    feat: AuthorizationFilter 수정
    - AccessToken 추출 및 확인 로직 변경

--- gateway/src/main/java/com/mongs/gateway/filter/AuthorizationFilter.java
+++ gateway/src/main/java/com/mongs/gateway/filter/AuthorizationFilter.java
@@ -1,5 +1,9 @@
 package com.mongs.gateway.filter;
 
+import com.mongs.gateway.exception.AuthorizationException;
+import com.mongs.gateway.exception.ErrorCode;
+import com.mongs.gateway.exception.TokenNotFoundException;
+import com.mongs.gateway.util.HttpUtils;
 import com.mongs.gateway.util.TokenProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
@@ -14,10 +18,12 @@ import java.util.Objects;
 public class AuthorizationFilter extends AbstractGatewayFilterFactory<FilterConfig> {
 
     private final TokenProvider tokenProvider;
+    private final HttpUtils httpUtils;
 
-    public AuthorizationFilter(TokenProvider tokenProvider) {
+    public AuthorizationFilter(TokenProvider tokenProvider, HttpUtils httpUtils) {
         super(FilterConfig.class);
         this.tokenProvider = tokenProvider;
+        this.httpUtils = httpUtils;
     }
 
     @Override
@@ -25,12 +31,12 @@ public class AuthorizationFilter extends AbstractGatewayFilterFactory<FilterConf
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
 
-            String accessToken = Objects.requireNonNull(request.getHeaders().get("Authorization")).get(0);
-
-            accessToken = accessToken.substring(7);
+            String accessToken = httpUtils.getHeader(request, "Authorization")
+                    .orElseThrow(() -> new TokenNotFoundException(ErrorCode.ACCESS_TOKEN_NOT_FOUND.getMessage()))
+                    .substring(7);
 
             if (tokenProvider.isTokenExpired(accessToken)) {
-                throw new RuntimeException("");
+                throw new AuthorizationException(ErrorCode.ACCESS_TOKEN_EXPIRED.getMessage());
             }
 
             if (config.preLogger) {
