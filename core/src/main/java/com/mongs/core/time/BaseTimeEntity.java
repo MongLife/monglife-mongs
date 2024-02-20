ed2eb9912cd5d9e1c0b1af5e10bcb072f01763d4
tableminpark <tablemin_park@daum.net>
2024-02-20 22:38:25 +0900

    fix: management 모듈 공통 코드 core 모듈로 이관
    - MongActiveCode, MongConditionCode 클래스 core 모듈로 이관
    - MapCode, MongCode 클래스 생성
    - Code Repository 생성
    - 현재 Enum 클래스로 선언하 사용하지만, 추후 외부 저장소로 이관 예정

--- core/src/main/java/com/mongs/core/time/BaseTimeEntity.java
+++ core/src/main/java/com/mongs/core/time/BaseTimeEntity.java
@@ -1,26 +1,24 @@
-package com.mongs.common.time;
+package com.mongs.core.time;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@EntityListeners(value = {AuditingEntityListener.class})
-@MappedSuperclass // 자식에 매핑(db에 반영x)
 @Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
-
+    @Column(updatable = false, nullable = false)
     @CreatedDate
-    @Column(updatable = false)
-    public LocalDateTime createTime;
-
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
     @LastModifiedDate
-    public LocalDateTime updateTime;
+    private LocalDateTime updatedAt;
 
 }
