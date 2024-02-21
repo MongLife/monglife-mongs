066b2b15ffccfb81366062ae3539d53fdae0b8b6
박상민 <87235273+tableMinPark@users.noreply.github.com>
2024-02-21 20:23:32 +0900

    컬렉션 엔티티 및 레포지토리 생성 (#69)
    - 맵 컬렉션, 몽 컬렉션 Entity, Repository 생성
    - H2 Console 접속을 위한 SecurityConfig 수정
    - codeRepository 로직 수정
    - 맵 컬렉션, 몽 컬렉션 조회 단위 테스트 작성
    - code 값 조회 단위 테스트 작성

--- collection/src/main/java/com/mongs/collection/config/SecurityConfig.java
+++ collection/src/main/java/com/mongs/collection/config/SecurityConfig.java
@@ -7,11 +7,14 @@ import com.mongs.core.security.exception.UnAuthorizationHandler;
 import com.mongs.core.security.filter.PassportFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,6 +27,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final ObjectMapper objectMapper;
+
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled", havingValue = "true")
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return web -> web.ignoring().requestMatchers(PathRequest.toH2Console());
+    }
 
     @Bean
     public SecurityFilterChain filterChain(
