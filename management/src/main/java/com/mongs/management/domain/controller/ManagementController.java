f92139db20c884f4fa549216ffbd87bc7d06a904
강민승 <k4minseung@gmail.com>
2024-02-21 23:49:08 +0900

    Rename : domain 내로 옮김으로 인한 commit

--- management/src/main/java/com/mongs/management/domain/controller/ManagementController.java
+++ management/src/main/java/com/mongs/management/domain/controller/ManagementController.java
@@ -1,15 +1,8 @@
 package com.mongs.management.domain.controller;
 
 import com.mongs.core.security.principal.PassportDetail;
-import com.mongs.management.domain.service.ManagementService;
-import com.mongs.management.domain.service.dto.CreateMong;
-import com.mongs.management.domain.service.dto.InitMong;
-import com.mongs.management.domain.service.dto.Stroke;
-import com.mongs.management.response.ResponseHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,35 +11,9 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/management")
 public class ManagementController {
-
-    private final ManagementService managementService;
-
     @GetMapping("/test")
     public String test(@AuthenticationPrincipal PassportDetail passportDetail) {
         // passport : 인증 객체 (gateway 로 부터 넘어오는 사용자 정보 값)
         return passportDetail.toString();
-    }
-
-    @PostMapping("/")
-    public ResponseEntity<ResponseHandler<CreateMong>> createMong(@RequestBody InitMong initMong,
-                                                                  @AuthenticationPrincipal PassportDetail passportDetail) {
-        return ResponseEntity
-                .ok()
-                .body(ResponseHandler.<CreateMong>builder()
-                        .message("몽 생성")
-                        .statusCode(HttpStatus.CREATED)
-                        .data(managementService.createMong(initMong, passportDetail.getId()))
-                        .build());
-    }
-
-    @PutMapping("/stroke")
-    public ResponseEntity<ResponseHandler<Stroke>> toMongStroke(@AuthenticationPrincipal PassportDetail passportDetail) {
-        return ResponseEntity
-                .ok()
-                .body(ResponseHandler.<Stroke>builder()
-                        .message("몽 쓰다듬기 성공")
-                        .statusCode(HttpStatus.ACCEPTED)
-                        .data(managementService.toMongStroke(passportDetail.getId()))
-                        .build());
     }
 }
--- management/src/main/java/com/mongs/management/domain/service/ManagementServiceImpl.java
+++ management/src/main/java/com/mongs/management/domain/service/ManagementServiceImpl.java
@@ -1,12 +1,12 @@
 package com.mongs.management.domain.service;
 
+import com.mongs.core.error.ErrorException;
 import com.mongs.management.domain.entity.Management;
 import com.mongs.management.domain.repository.ManagementRepository;
 import com.mongs.management.domain.service.dto.*;
 import com.mongs.management.exception.ManagementErrorCode;
 import com.mongs.management.exception.ManagementException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
@@ -16,7 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.Random;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -25,33 +24,14 @@ public class ManagementServiceImpl implements ManagementService {
     private final ManagementRepository managementRepository;
 
 
-    @Retryable(value = DataAccessException.class, maxAttempts = 3, backoff = @Backoff(delay = 1000))
     @Override
     public CreateMong createMong(InitMong initmong, Long memberId) {
-
-        Boolean sleep = initmong.sleepEnd().isAfter(LocalDateTime.now()) &&
-                initmong.sleepStart().isBefore(LocalDateTime.now());
-
-        Management mong = Management.builder()
-                .memberId(memberId)
-                .name(initmong.name())
-                .sleepStart(initmong.sleepStart())
-                .sleepEnd(initmong.sleepEnd())
-                .weight(new Random().nextDouble() * 100)
-                .sleep(sleep)
-                .build();
-        managementRepository.save(mong);
-        return CreateMong.of(mong);
-
+        return null;
     }
 
     @Override
     public Stroke toMongStroke(Long memberId) {
-        Management mong = managementRepository.findManagementByMemberId(memberId)
-                .orElseThrow(() -> new ManagementException(ManagementErrorCode.NOT_FOUND));
-
-        mong.doStroke(mong.getStrokeCount());
-        return Stroke.of(mong);
+        return null;
     }
 
     @Override
