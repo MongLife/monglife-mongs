1379052e7db080bb57e941a82cca5ad62759b459
강민승 <k4minseung@gmail.com>
2024-02-22 01:46:02 +0900

    feat : create mong (service, controller, testcode)

--- management/src/main/java/com/mongs/management/domain/controller/ManagementController.java
+++ management/src/main/java/com/mongs/management/domain/controller/ManagementController.java
@@ -1,8 +1,14 @@
 package com.mongs.management.domain.controller;
 
 import com.mongs.core.security.principal.PassportDetail;
+import com.mongs.management.domain.service.ManagementService;
+import com.mongs.management.domain.service.dto.CreateMong;
+import com.mongs.management.domain.service.dto.InitMong;
+import com.mongs.management.response.ResponseHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +17,24 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/management")
 public class ManagementController {
+
+    private final ManagementService managementService;
+
     @GetMapping("/test")
     public String test(@AuthenticationPrincipal PassportDetail passportDetail) {
         // passport : 인증 객체 (gateway 로 부터 넘어오는 사용자 정보 값)
         return passportDetail.toString();
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ResponseHandler<CreateMong>> createMong(@RequestBody InitMong initMong,
+                                                                  @AuthenticationPrincipal PassportDetail passportDetail) {
+        return ResponseEntity
+                .ok()
+                .body(ResponseHandler.<CreateMong>builder()
+                        .message("몽 생성")
+                        .statusCode(HttpStatus.CREATED)
+                        .data(managementService.createMong(initMong, passportDetail.getId()))
+                        .build());
     }
 }
