0be14379afa1af90111ccefc9cc7dee684765adc
강민승 <k4minseung@gmail.com>
2024-02-22 23:45:31 +0900

    feat : mong stroke method, service단 단위테스트 완료

--- management/src/main/java/com/mongs/management/domain/repository/ManagementRepository.java
+++ management/src/main/java/com/mongs/management/domain/repository/ManagementRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ManagementRepository extends JpaRepository<Management, Long> {
+    Optional<Management> findManagementByMemberId (Long memberId);
 }
