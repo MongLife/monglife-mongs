31dc2c8a29fb2b3e4e7fc56cbdaa12238a4ad91d
tableMinPark <tablemin_park@daum.net>
2024-02-23 13:54:13 +0900

    fix: Collection 모듈 에러 메시지 수정
    - INVALID_MONG_CODE 에러 메시지 수정

--- collection/src/main/java/com/mongs/collection/exception/CollectionErrorCode.java
+++ collection/src/main/java/com/mongs/collection/exception/CollectionErrorCode.java
@@ -11,8 +11,6 @@ public enum CollectionErrorCode implements ErrorCode {
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "COLLECTION-100", "Invalid Parameter"),
     INVALID_MAP_CODE(HttpStatus.BAD_REQUEST, "COLLECTION-101", "Invalid Map Code"),
     INVALID_MONG_CODE(HttpStatus.BAD_REQUEST, "COLLECTION-102", "Invalid Map Code"),
-//    NOT_FOUND_MAP_CODE(HttpStatus.BAD_REQUEST, "COLLECTION-103", "Not Found Map Code"),
-//    NOT_FOUND_MONG_CODE(HttpStatus.BAD_REQUEST, "COLLECTION-104", "Not Found Mong Code")
     ;
 
     private final HttpStatus httpStatus;
