b604734e0d7a5ac074cb1d82499cb7fc368cc13d
강민승 <k4minseung@gmail.com>
2024-02-25 00:41:40 +0900

    feat : test code create for controller test

---
+++ management/src/test/java/com/mongs/management/domain/security/WithMockCustomUserSecurityContextFactory.java
@@ -0,0 +1,39 @@
+package com.mongs.management.domain.security;
+
+import com.mongs.core.passport.PassportData;
+import com.mongs.core.passport.PassportMember;
+import com.mongs.core.passport.PassportVO;
+import com.mongs.core.security.principal.PassportDetail;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import java.util.UUID;
+
+public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockPassportDetail> {
+    @Override
+    public SecurityContext createSecurityContext(WithMockPassportDetail passportDetail) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+        PassportVO passportVO = PassportVO.builder()
+                .data(PassportData.builder()
+                        .member(PassportMember.builder()
+                                .id(1L)
+                                .email("test@test.com")
+                                .name("testName")
+                                .role("NORMAL")
+                                .build())
+                        .build())
+                .passportIntegrity("passportIntegrity")
+                .build();
+
+        PassportDetail principal = new PassportDetail(passportVO);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(principal, UUID.randomUUID().toString(), principal.getAuthorities());
+
+        context.setAuthentication(authentication);
+
+        return context;
+    }
+}
---
+++ management/src/test/java/com/mongs/management/domain/security/WithMockPassportDetail.java
@@ -0,0 +1,15 @@
+package com.mongs.management.domain.security;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockPassportDetail {
+    long id() default 1L;
+    String email() default "test@test.com";
+    String name() default "testName";
+    String role() default "NORMAL";
+}
