0cc575cd4384b4f3e5ea845adae25e9810e76e10
강민승 <k4minseung@gmail.com>
2024-02-27 22:55:07 +0900

    Revert "feat : 새롭게 진행된 회의에서 도출된 도메인과 필드 개발"
    This reverts commit f7bc61570162ac6a7ac89eb4fb28eadf940749cb.

--- management/src/main/java/com/mongs/management/domain/ateFood/AteFoodHistoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mongs.management.domain.ateFood;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AteFoodHistoryRepository extends JpaRepository<AteFoodHistory, Long> {
-}
--- management/src/main/java/com/mongs/management/domain/food/FoodRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mongs.management.domain.food;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodRepository extends JpaRepository<Food, Long> {
-}
