f92609ad2af60feae3d3b9876b1352e644ca9625
강민승 <k4minseung@gmail.com>
2024-02-27 22:56:26 +0900

    Revert "config : scheduler Enable Config set"
    This reverts commit e73025bf8c37abc28027abda3bda89c487f8f088.

--- management/src/main/java/com/mongs/management/ManagementApplication.java
+++ management/src/main/java/com/mongs/management/ManagementApplication.java
@@ -2,9 +2,7 @@ package com.mongs.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class ManagementApplication {
 
--- management/src/main/java/com/mongs/management/domain/mongEvent/MongEventRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mongs.management.domain.mongEvent;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MongEventRepository extends JpaRepository<MongEvent, Long> {
-}
