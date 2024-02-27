e73025bf8c37abc28027abda3bda89c487f8f088
강민승 <k4minseung@gmail.com>
2024-02-27 22:52:02 +0900

    config : scheduler Enable Config set

--- management/src/main/java/com/mongs/management/domain/mong/entity/Mong.java
+++ management/src/main/java/com/mongs/management/domain/mong/entity/Mong.java
@@ -4,11 +4,10 @@ import com.mongs.core.code.MongCode;
 import com.mongs.core.code.MongConditionCode;
 import com.mongs.core.time.BaseTimeEntity;
 import com.mongs.management.domain.ateFood.AteFoodHistory;
+import com.mongs.management.domain.mong.service.dto.MongStatus;
+import com.mongs.management.domain.mong.service.enums.MongCollapse;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
 
 import java.util.List;
@@ -90,5 +89,38 @@ public class Mong extends BaseTimeEntity {
 
     public void setNumberOfTraining(int trainingCount) {
         this.numberOfTraining += trainingCount;
+    }
+
+    public MongStatus mongPassedTime(boolean minusSleep, int value) {
+        // minusSleep가 true일 경우에만 healthy 감소하되, 0 이하로 내려가지 않도록 처리
+        if (minusSleep) {
+            this.healthy = Math.max(0, this.healthy - value);
+        }
+
+        // satiety와 strength 감소하되, 0 이하로 내려가지 않도록 처리
+        this.satiety = Math.max(0, this.satiety - value);
+        this.strength = Math.max(0, this.strength - value);
+
+        // MongCollapse 상태 결정
+        MongCollapse collapse = null;
+        if (this.healthy == 0) {
+            collapse = MongCollapse.NO_STAMINA;
+        } else if (this.satiety == 0) {
+            collapse = MongCollapse.STARVING;
+        }
+
+        return MongStatus.builder()
+                .mongId(this.id)
+                .collapse(collapse) // collapse가 null일 경우에도 Lombok @Builder는 null 값을 처리할 수 있음.
+                .build();
+    }
+
+
+    public void stateToDeath() {
+        this.state = MongConditionCode.DIE;
+    }
+
+    public void addPoop() {
+        this.numberOfPoop += 1;
     }
 }
