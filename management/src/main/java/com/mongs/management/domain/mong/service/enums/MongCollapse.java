3e22dfe8a6877e9bab6f443eba29cdedc9c8ede6
Kang Minseung <119389068+donowhy@users.noreply.github.com>
2024-02-28 14:23:57 +0900

    몽 스케줄러 개발 ( 추가 변경 필 ) (#102)
    - 몽 스케줄러 ( 시간지남, 이벤트 순회, 수정시간 대비 1시간 이후)

---
+++ management/src/main/java/com/mongs/management/domain/mong/service/enums/MongCollapse.java
@@ -0,0 +1,18 @@
+package com.mongs.management.domain.mong.service.enums;
+
+import com.mongs.core.code.Code;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MongCollapse implements Code{
+
+    STARVING("EV000", "공복"),
+    NO_STAMINA("EV001", "체력0"),
+    NORMAL("EV002", "일상생활");
+
+    private final String groupCode = "EV";
+    private final String code;
+    private final String name;
+}
