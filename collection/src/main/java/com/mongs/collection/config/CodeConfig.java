ef75c6d1e6291bc36b4f44be88d17dee6e26bd1d
강민승 <k4minseung@gmail.com>
2024-03-03 15:59:08 +0900

    fix : 참조 클래스 없음으로 인한 우선 주석화

--- collection/src/main/java/com/mongs/collection/config/CodeConfig.java
+++ collection/src/main/java/com/mongs/collection/config/CodeConfig.java
@@ -1,15 +1,15 @@
 package com.mongs.collection.config;
 
-import com.mongs.core.code.CodeRepository;
-import com.mongs.core.code.CodeRepositoryImpl;
+//import com.mongs.core.code.CodeRepository;
+//import com.mongs.core.code.CodeRepositoryImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CodeConfig {
 
-    @Bean
-    public CodeRepository codeRepository() {
-        return new CodeRepositoryImpl();
-    }
+//    @Bean
+//    public CodeRepository codeRepository() {
+//        return new CodeRepositoryImpl();
+//    }
 }
