ae1e65fee25e9afe7d0fde3b95c37ff934df9009
강민승 <k4minseung@gmail.com>
2024-03-03 16:01:56 +0900

    Feat : 음식 관련 전체 조회 및 카테고리별 조회, Querydsl 사용

--- management/src/main/java/com/mongs/management/domain/food/FoodRepository.java
+++ management/src/main/java/com/mongs/management/domain/food/FoodRepository.java
@@ -1,6 +1,14 @@
 package com.mongs.management.domain.food;
 
+import com.mongs.management.domain.food.Query.CustomFoodRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface FoodRepository extends JpaRepository<Food, Long> {
+import java.util.List;
+
+public interface FoodRepository extends JpaRepository<Food, Long>, CustomFoodRepository {
+
+    @Query("select f from Food as f where f.foodCode = :foodCode")
+    Food findFood (@Param("foodCode") String foodCode);
 }
