0c99eaeea86716ef2f9f86a8b150b197adad759f
박상민 <87235273+tableMinPark@users.noreply.github.com>
2024-03-03 13:40:08 +0900

    공통 코드 변경 사항 수정 및 Redis 연결 실패 예외 처리 로직 추가 (#118)
    - 공통 코드 변경 사항에 따른 collection 모듈 코드 수정
    - Redis 연결 실패 시, 예외 처리 핸들러 생성 (Auth 모듈)

--- auth/src/main/java/com/mongs/auth/exception/AuthErrorCode.java
+++ auth/src/main/java/com/mongs/auth/exception/AuthErrorCode.java
@@ -12,7 +12,9 @@ public enum AuthErrorCode implements ErrorCode {
     REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH-101", "RefreshToken Expired"),
     ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH-102", "AccessToken Expired"),
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "AUTH-103", "Not Found Member"),
-    PASSPORT_GENERATE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH-104", "Passport Generate Fail");
+    PASSPORT_GENERATE_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH-104", "Passport Generate Fail"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "AUTH-105", "Internal Server Error")
+    ;
 
     private final HttpStatus httpStatus;
     private final String code;
