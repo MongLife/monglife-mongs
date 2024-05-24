78d700b619e134860f500d0eaabfc2113e7cd8f7
박상민A <tableminpark@wisenut.co.kr>
2024-05-24 16:15:51 +0900

    fix: feign client 오류 수정

--- modules/play-feign-module/src/main/java/com/mongs/play/module/feign/client/ManagementInternalClient.java
+++ modules/play-feign-module/src/main/java/com/mongs/play/module/feign/client/ManagementInternalClient.java
@@ -6,6 +6,8 @@ import com.mongs.play.module.feign.dto.req.*;
 import com.mongs.play.module.feign.dto.res.*;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -23,4 +25,25 @@ public interface ManagementInternalClient {
     ResponseEntity<DeadResDto> dead(@RequestBody DeadReqDto deadReqDto);
     @PutMapping("/internal/management/increasePayPoint")
     ResponseEntity<IncreasePayPointResDto> increasePayPoint(@RequestBody IncreasePayPointReqDto increasePayPointReqDto);
+
+    @PostMapping("/worker/management/zeroEvolution")
+    ResponseEntity<EvolutionScheduleResDto> zeroEvolutionSchedule(@RequestBody EvolutionScheduleReqDto evolutionScheduleReqDto);
+    @PostMapping("/worker/management/firstEvolution")
+    ResponseEntity<EvolutionScheduleResDto> firstEvolutionSchedule(@RequestBody EvolutionScheduleReqDto evolutionScheduleReqDto);
+    @PostMapping("/worker/management/evolution")
+    ResponseEntity<EvolutionScheduleResDto> evolutionSchedule(@RequestBody EvolutionScheduleReqDto evolutionScheduleReqDto);
+    @PostMapping("/worker/management/lastEvolution")
+    ResponseEntity<EvolutionScheduleResDto> lastEvolutionSchedule(@RequestBody EvolutionScheduleReqDto evolutionScheduleReqDto);
+    @PostMapping("/worker/management/sleeping")
+    ResponseEntity<SleepingScheduleResDto> sleepingSchedule(SleepingScheduleReqDto sleepingScheduleReqDto);
+    @PostMapping("/worker/management/delete")
+    ResponseEntity<DeleteScheduleResDto> deleteSchedule(DeleteScheduleReqDto deleteScheduleReqDto);
+    @PostMapping("/worker/management/dead/healthy")
+    ResponseEntity<DeadScheduleResDto> deadHealthySchedule(DeadScheduleReqDto deadScheduleReqDto);
+    @DeleteMapping("/worker/management/dead/healthy")
+    ResponseEntity<DeadScheduleResDto> deadHealthyScheduleStop(DeadScheduleReqDto deadScheduleReqDto);
+    @PostMapping("/worker/management/dead/satiety")
+    ResponseEntity<DeadScheduleResDto> deadSatietySchedule(DeadScheduleReqDto deadScheduleReqDto);
+    @DeleteMapping("/worker/management/dead/satiety")
+    ResponseEntity<DeadScheduleResDto> deadSatietyScheduleStop(DeadScheduleReqDto deadScheduleReqDto);
 }
--- modules/play-feign-module/src/main/java/com/mongs/play/module/feign/service/ManagementWorkerFeignService.java
+++ modules/play-feign-module/src/main/java/com/mongs/play/module/feign/service/ManagementWorkerFeignService.java
@@ -1,6 +1,6 @@
 package com.mongs.play.module.feign.service;
 
-import com.mongs.play.module.feign.client.ManagementWorkerClient;
+import com.mongs.play.module.feign.client.ManagementInternalClient;
 import com.mongs.play.module.feign.dto.req.*;
 import com.mongs.play.module.feign.dto.res.*;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +12,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ManagementWorkerFeignService {
 
-    private final ManagementWorkerClient managementWorkerClient;
+    private final ManagementInternalClient managementInternalClient;
 
     public EvolutionScheduleResDto zeroEvolutionSchedule(Long mongId) {
-        var res = managementWorkerClient.zeroEvolutionSchedule(EvolutionScheduleReqDto.builder()
+        var res = managementInternalClient.zeroEvolutionSchedule(EvolutionScheduleReqDto.builder()
                 .mongId(mongId)
                 .build());
 
@@ -23,7 +23,7 @@ public class ManagementWorkerFeignService {
     }
 
     public EvolutionScheduleResDto firstEvolutionSchedule(Long mongId) {
-        var res = managementWorkerClient.firstEvolutionSchedule(EvolutionScheduleReqDto.builder()
+        var res = managementInternalClient.firstEvolutionSchedule(EvolutionScheduleReqDto.builder()
                 .mongId(mongId)
                 .build());
 
@@ -31,7 +31,7 @@ public class ManagementWorkerFeignService {
     }
 
     public EvolutionScheduleResDto evolutionSchedule(Long mongId) {
-        var res = managementWorkerClient.evolutionSchedule(EvolutionScheduleReqDto.builder()
+        var res = managementInternalClient.evolutionSchedule(EvolutionScheduleReqDto.builder()
                 .mongId(mongId)
                 .build());
 
@@ -39,7 +39,7 @@ public class ManagementWorkerFeignService {
     }
 
     public EvolutionScheduleResDto lastEvolutionSchedule(Long mongId) {
-        var res = managementWorkerClient.lastEvolutionSchedule(EvolutionScheduleReqDto.builder()
+        var res = managementInternalClient.lastEvolutionSchedule(EvolutionScheduleReqDto.builder()
                 .mongId(mongId)
                 .build());
 
@@ -47,7 +47,7 @@ public class ManagementWorkerFeignService {
     }
 
     public SleepingScheduleResDto sleepingSchedule(Long mongId, Boolean isSleeping) {
-        var res = managementWorkerClient.sleepingSchedule(SleepingScheduleReqDto.builder()
+        var res = managementInternalClient.sleepingSchedule(SleepingScheduleReqDto.builder()
                 .mongId(mongId)
                 .isSleeping(isSleeping)
                 .build());
@@ -56,7 +56,7 @@ public class ManagementWorkerFeignService {
     }
 
     public DeleteScheduleResDto deleteSchedule(Long mongId) {
-        var res = managementWorkerClient.deleteSchedule(DeleteScheduleReqDto.builder()
+        var res = managementInternalClient.deleteSchedule(DeleteScheduleReqDto.builder()
                 .mongId(mongId)
                 .build());
 
@@ -64,7 +64,7 @@ public class ManagementWorkerFeignService {
     }
 
     public DeadScheduleResDto deadHealthySchedule(Long mongId) {
-        var res = managementWorkerClient.deadHealthySchedule(DeadScheduleReqDto.builder()
+        var res = managementInternalClient.deadHealthySchedule(DeadScheduleReqDto.builder()
                 .mongId(mongId)
                 .build());
 
@@ -72,7 +72,7 @@ public class ManagementWorkerFeignService {
     }
 
     public DeadScheduleResDto deadHealthyScheduleStop(Long mongId) {
-        var res = managementWorkerClient.deadHealthyScheduleStop(DeadScheduleReqDto.builder()
+        var res = managementInternalClient.deadHealthyScheduleStop(DeadScheduleReqDto.builder()
                 .mongId(mongId)
                 .build());
 
@@ -80,7 +80,7 @@ public class ManagementWorkerFeignService {
     }
 
     public DeadScheduleResDto deadSatietySchedule(Long mongId) {
-        var res = managementWorkerClient.deadSatietySchedule(DeadScheduleReqDto.builder()
+        var res = managementInternalClient.deadSatietySchedule(DeadScheduleReqDto.builder()
                 .mongId(mongId)
                 .build());
 
@@ -88,7 +88,7 @@ public class ManagementWorkerFeignService {
     }
 
     public DeadScheduleResDto deadSatietyScheduleStop(Long mongId) {
-        var res = managementWorkerClient.deadSatietyScheduleStop(DeadScheduleReqDto.builder()
+        var res = managementInternalClient.deadSatietyScheduleStop(DeadScheduleReqDto.builder()
                 .mongId(mongId)
                 .build());
 
