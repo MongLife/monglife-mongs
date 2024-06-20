3574927777baf1071138cb892d30752f51f3b053
tableminpark <tablemin_park@daum.net>
2024-06-20 23:48:09 +0900

    fix: 배틀 모듈 Enter 부분 디버깅 진행중

--- apps/play-battle-app-worker/src/main/java/com/mongs/play/app/battle/worker/event/BattleWorkerEventListener.java
+++ apps/play-battle-app-worker/src/main/java/com/mongs/play/app/battle/worker/event/BattleWorkerEventListener.java
@@ -4,6 +4,7 @@ import com.mongs.play.app.battle.worker.service.BattleMatchService;
 import com.mongs.play.client.publisher.battle.event.MatchExitEvent;
 import com.mongs.play.client.publisher.battle.event.MatchPickEvent;
 import com.mongs.play.client.publisher.battle.event.MatchEnterEvent;
+import com.mongs.play.core.exception.app.BattleWorkerException;
 import com.mongs.play.domain.battle.code.PickCode;
 import com.mongs.play.domain.battle.vo.BattleRoomVo;
 import lombok.RequiredArgsConstructor;
@@ -22,27 +23,39 @@ public class BattleWorkerEventListener {
     @Async
     @EventListener
     public void matchEnter(MatchEnterEvent event) {
-        String roomId = event.roomId();
-        String playerId = event.playerId();
-        battleMatchService.matchEnter(roomId, playerId);
+        try {
+            String roomId = event.roomId();
+            String playerId = event.playerId();
+            battleMatchService.matchEnter(roomId, playerId);
+        } catch (BattleWorkerException e) {
+            log.error("[BattleWorkerEventListener] match enter error : {}", e.getMessage());
+        }
     }
 
     @Async
     @EventListener
     public void matchPick(MatchPickEvent event) {
-        String roomId = event.roomId();
-        String playerId = event.playerId();
-        String targetPlayerId = event.targetPlayerId();
-        PickCode pick = PickCode.valueOf(event.pick());
-        BattleRoomVo battleRoomVo = battleMatchService.matchPick(roomId, playerId, targetPlayerId, pick);
-        battleMatchService.matchOver(battleRoomVo);
+        try {
+            String roomId = event.roomId();
+            String playerId = event.playerId();
+            String targetPlayerId = event.targetPlayerId();
+            PickCode pick = PickCode.valueOf(event.pick());
+            BattleRoomVo battleRoomVo = battleMatchService.matchPick(roomId, playerId, targetPlayerId, pick);
+            battleMatchService.matchOver(battleRoomVo);
+        } catch (BattleWorkerException e) {
+            log.error("[BattleWorkerEventListener] match pick error : {}", e.getMessage());
+        }
     }
 
     @Async
     @EventListener
     public void matchExit(MatchExitEvent event) {
-        String roomId = event.roomId();
-        String playerId = event.playerId();
-        battleMatchService.matchExit(roomId, playerId);
+        try {
+            String roomId = event.roomId();
+            String playerId = event.playerId();
+            battleMatchService.matchExit(roomId, playerId);
+        } catch (BattleWorkerException e) {
+            log.error("[BattleWorkerEventListener] match exit error : {}", e.getMessage());
+        }
     }
 }
