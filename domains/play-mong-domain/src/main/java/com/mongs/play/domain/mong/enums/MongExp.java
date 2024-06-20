fe86244620ba59a02fe560865fd0e9cc96816592
tableminpark <tablemin_park@daum.net>
2024-06-21 00:27:36 +0900

    fix: 배틀 모듈 Enter 부분 디버깅 진행중

--- domains/play-mong-domain/src/main/java/com/mongs/play/domain/mong/enums/MongExp.java
+++ domains/play-mong-domain/src/main/java/com/mongs/play/domain/mong/enums/MongExp.java
@@ -6,10 +6,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum MongExp {
 
-    STROKE(5D),
+    STROKE(1D),
     ATTENDANCE(5D),
-    CLEANING_POOP(1D),
-    EAT_THE_FOOD(10D)
+    CLEANING_POOP(2D),
+    EAT_THE_FOOD(3D)
     ;
 
     public final Double exp;
