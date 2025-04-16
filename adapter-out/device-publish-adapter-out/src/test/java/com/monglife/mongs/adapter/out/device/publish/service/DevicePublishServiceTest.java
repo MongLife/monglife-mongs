cfd10de2cf4b294f9e709dfb9388788be81aa40d
tableminpark <tablemin_park@daum.net>
2025-04-16 16:57:47 +0900

    fix: 걸음 수 비동기 응답 단위 테스트 코드 수정
    - 기존 Thread.sleep 사용하는 방식에서 CountDownLatch.await() 활용하는 방식으로 수정
    - 단위 테스트용 Consumer Component 별도의 파일로 분리

--- adapter-out/device-publish-adapter-out/src/test/java/com/monglife/mongs/adapter/out/device/publish/service/DevicePublishServiceTest.java
+++ adapter-out/device-publish-adapter-out/src/test/java/com/monglife/mongs/adapter/out/device/publish/service/DevicePublishServiceTest.java
@@ -1,11 +1,6 @@
 package com.monglife.mongs.adapter.out.device.publish.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.monglife.core.dto.response.ResponseDto;
-import com.monglife.module.mqtt.annotation.MqttConsumer;
-import com.monglife.module.mqtt.annotation.MqttMapping;
-import com.monglife.module.mqtt.annotation.MqttPayload;
+import com.monglife.core.utils.CommonUtil;
 import com.monglife.module.mqtt.config.MqttAutoConfig;
 import com.monglife.mongs.adapter.out.device.publish.config.AdapterOutDevicePublishConfig;
 import com.monglife.mongs.adapter.out.device.publish.dto.response.DevicePublishDto;
@@ -15,36 +10,35 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @EnableAutoConfiguration
 @ActiveProfiles("test")
-@TestPropertySource(locations = "classpath:application-test.yml")
+@TestPropertySource(locations = "classpath:application.yml")
 @ContextConfiguration(classes = { AdapterOutDevicePublishConfig.class, MqttAutoConfig.class })
 class DevicePublishServiceTest {
 
-    private static final Logger log = LoggerFactory.getLogger(DevicePublishServiceTest.class);
-
     private final DevicePublishPort devicePublishPort;
 
+    private final Consumer consumer;
+
     @Autowired
-    public DevicePublishServiceTest(DevicePublishPort devicePublishPort) {
+    public DevicePublishServiceTest(DevicePublishPort devicePublishPort, Consumer consumer) {
         this.devicePublishPort = devicePublishPort;
+        this.consumer = consumer;
     }
 
 
@@ -52,15 +46,12 @@ class DevicePublishServiceTest {
     @DisplayName("보유 걸음 수 비동기 응답 단위 테스트")
     class publishCurrentWalkingCountPort {
 
-        private static Queue<Map<String, DevicePublishDto>> consumeMessageQueue;
-
         protected static String deviceId;
         private static final LocalDateTime deviceBootedDt = LocalDateTime.of(2025, 1, 1, 0, 0);
 
         @BeforeEach
         void beforeEach() {
-            deviceId = UUID.randomUUID().toString().replace("-", "");
-            consumeMessageQueue = new ArrayDeque<>();
+            deviceId = CommonUtil.randomId();
         }
 
         @Test
@@ -77,45 +68,21 @@ class DevicePublishServiceTest {
                     .deviceBootedDt(deviceBootedDt)
                     .build();
 
+            DevicePublishDto devicePublishDto = new DevicePublishDto();
+            CountDownLatch countDownLatch = new CountDownLatch(1);
+
+            consumer.reset(deviceId, devicePublishDto, countDownLatch);
+
             // act
             devicePublishPort.publishCurrentWalkingCountPort(step);
 
-            Thread.sleep(1000);
-
-            int consumeMessageQueueSize = consumeMessageQueue.size();
-            Map<String, DevicePublishDto> consumeMessageMap = consumeMessageQueue.poll();
-            DevicePublishDto devicePublishDto = consumeMessageMap != null ? consumeMessageMap.get(deviceId) : null;
+            boolean messageConsumed = countDownLatch.await(5, TimeUnit.SECONDS);
 
             // assert
-            assertEquals(1, consumeMessageQueueSize);
-            assertNotNull(devicePublishDto);
+            assertTrue(messageConsumed);
             assertEquals(deviceId, devicePublishDto.getDeviceId());
             assertEquals(walkingCount, devicePublishDto.getWalkingCount());
             assertEquals(consumeWalkingCount, devicePublishDto.getConsumeWalkingCount());
-        }
-
-        /**
-         * Mqtt Consume Component
-         * @hidden
-         */
-        @MqttConsumer
-        private static class Consumer {
-
-            private final ObjectMapper objectMapper;
-
-            public Consumer(ObjectMapper objectMapper) {
-                this.objectMapper = objectMapper;
-                this.objectMapper.registerModule(new JavaTimeModule());
-            }
-
-            @MqttMapping("/device/{id}")
-            public void consumeDevice(@PathVariable("id") String id, @MqttPayload ResponseDto<Map<String, Objects>> payload) {
-                // mqtt library 는 제네릭 타입 Payload 를 변환할 수 없음 (직접 변환)
-                DevicePublishDto devicePublishDto = objectMapper.convertValue(payload.getResult(), DevicePublishDto.class);
-                if (devicePublishDto != null && id.equals(deviceId)) {
-                    consumeMessageQueue.offer(Map.of(deviceId, devicePublishDto));
-                }
-            }
         }
     }
 }
