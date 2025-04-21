1984c2ad96d0e5011a7e1d310b7ebd59748be96c
tableminpark <tablemin_park@daum.net>
2025-04-22 00:03:50 +0900

    fix: 의존성 설정 수정
    - 환경 설정 파일 복사 로직 수정
    - core 모듈 버전 변경

--- bootstrap/user-bootstrap-api/src/main/java/com/monglife/mongs/bootstrap/user/UserApplication.java
+++ bootstrap/user-bootstrap-api/src/main/java/com/monglife/mongs/bootstrap/user/UserApplication.java
@@ -1,16 +1,18 @@
 package com.monglife.mongs.bootstrap.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+@Slf4j
 @SpringBootApplication
 @Import(value = {})
 public class UserApplication {
 
     public static void main(String[] args) {
 
-        System.setProperty("spring.config.name", "application,adapter,core");
+        System.setProperty("spring.config.name", "application,event,persistence,publish,payment");
 
         SpringApplication.run(UserApplication.class, args);
     }
