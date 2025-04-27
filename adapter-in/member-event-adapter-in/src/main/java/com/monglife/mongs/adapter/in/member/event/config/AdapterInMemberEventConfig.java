cfe8258bbbd07ee26c6ba05c705194de07044dfd
tableminpark <tablemin_park@daum.net>
2025-04-27 15:26:55 +0900

    feat: member event adapter in 모듈 개발
    - 스타 포인트 환전 분산 트랜잭션 롤백 처리를 위한 컨슈머 생성
    - 컬렉션 몽 등록 분산 트랜잭션 처리를 위한 컨슈머 생성
    - kafka consume 단위 테스트 코드 작성

---
+++ adapter-in/member-event-adapter-in/src/main/java/com/monglife/mongs/adapter/in/member/event/config/AdapterInMemberEventConfig.java
@@ -0,0 +1,9 @@
+package com.monglife.mongs.adapter.in.member.event.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({ "com.monglife.mongs.adapter.in.member.event", "com.monglife.module" })
+public class AdapterInMemberEventConfig {
+}
---
+++ adapter-in/member-event-adapter-in/src/main/java/com/monglife/mongs/adapter/in/member/event/player/consumer/PlayerRollbackConsumer.java
@@ -0,0 +1,33 @@
+package com.monglife.mongs.adapter.in.member.event.player.consumer;
+
+import com.monglife.module.common.kafka.event.TransactionEvent;
+import com.monglife.mongs.adapter.in.member.event.transaction.ExchangeStarPointEventDto;
+import com.monglife.mongs.application.member.port.in.PlayerUseCase;
+import com.monglife.mongs.application.member.port.in.command.IncreaseStarPointCommand;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PlayerRollbackConsumer {
+
+    private final PlayerUseCase playerUseCase;
+
+    /**
+     * 스타 포인트 환전 트랜잭션 롤백 이벤트 리스너
+     * @param event 스타 포인트 환전 정보
+     */
+    @KafkaListener(topics = "rollback.exchangeStarPoint")
+    public void exchangeStarPointRollbackEvent(TransactionEvent<ExchangeStarPointEventDto> event) {
+
+        if (event.getData() != null) {
+            IncreaseStarPointCommand command = IncreaseStarPointCommand.builder()
+                    .accountId(event.getData().getAccountId())
+                    .starPoint(event.getData().getStarPoint())
+                    .build();
+
+            playerUseCase.increaseStarPointUseCase(command);
+        }
+    }
+}
