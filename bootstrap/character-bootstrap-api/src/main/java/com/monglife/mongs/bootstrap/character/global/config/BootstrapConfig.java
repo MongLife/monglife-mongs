295195b6313031e4fb574f9378c5bd7a02a1956d
tableminpark <tablemin_park@daum.net>
2025-05-16 09:47:17 +0900

    feat: Character API 부트 스트랩 생성

---
+++ bootstrap/character-bootstrap-api/src/main/java/com/monglife/mongs/bootstrap/character/global/config/BootstrapConfig.java
@@ -0,0 +1,9 @@
+package com.monglife.mongs.bootstrap.character.global.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.monglife.mongs.**")
+public class BootstrapConfig {
+}
