f5c05d267ea729d260d8c474251aae7ed2ad847c
tableminpark <tablemin_park@daum.net>
2025-08-11 21:52:25 +0900

    fix: 배틀 봇 플레이어 난이도 상향 조정

--- character-service/domain/battle-domain/src/main/java/com/monglife/mongs/domain/battle/enums/BotMatchMongType.java
+++ character-service/domain/battle-domain/src/main/java/com/monglife/mongs/domain/battle/enums/BotMatchMongType.java
@@ -7,12 +7,24 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum BotMatchMongType {
 
-    CH100("CH100", "별몽"),
-    CH101("CH101", "동글몽"),
-    CH102("CH102", "네몽")
+    CH100("CH100", "별몽", 50D, 50D, 50D),
+    CH101("CH101", "동글몽", 50D, 50D, 50D),
+    CH102("CH102", "네몽", 50D, 50D, 50D),
+    CH200("CH200", "안씻은 별별몽", 150D, 150D, 150D),
+    CH201("CH201", "안씻은 동글몽", 150D, 150D, 150D),
+    CH202("CH202", "안씻은 나네몽", 150D, 150D, 150D),
+    CH210("CH210", "무난한 별별몽", 250D, 250D, 250D),
+    CH211("CH211", "무난한 동글몽", 250D, 250D, 250D),
+    CH212("CH212", "무난한 나네몽", 250D, 250D, 250D),
     ;
 
     private final String mongCode;
 
     private final String mongName;
+
+    private final Double strength;
+
+    private final Double fatigue;
+
+    private final Double weight;
 }
